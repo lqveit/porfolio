df59428417c5eb053579cc4550adc3ef5e765924
lqveit <165130666+lqveit@users.noreply.github.com>
2024-04-11 01:51:35 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+   	loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    	autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	  })
+
+$('#banner1').owlCarousel({
+   	loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    	autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	  })
+
+$('#banner2').owlCarousel({
+   	loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    	autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	  })
+
+var typed = new Typed('.element', {
+               	strings: ["Graphic Designer.", "UX/UI Designer.","Gamer.","Content Creator."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+new WOW().init();
